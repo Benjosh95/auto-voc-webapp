cafa6e55cfa8b90d53004a652a423044ba9e704f
Benjosh95 <benjamin.fuertsch@inovex.de>
2024-08-10 19:08:44 +0200

    fix: build - suppress unused import warning

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,7 +1,9 @@
+// @ts-expect-error
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Footer from './Footer';
+
 
 describe('Footer', () => {
   it('renders the footer component', () => {
